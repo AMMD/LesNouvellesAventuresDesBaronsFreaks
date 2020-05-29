a902b785228ee1db6ab4b13c3514fc96de5383b7
orlammd <orl@ammd.net>
2020-05-29 12:12:09 +0200

    scripts pour copie locale des fichiers depuis le portable vers HDD externe

---
+++ Technique/Scripts/Synchro/src2MobileHDD.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Scripts pour synchroniser les nouveaux rushes vidéos vers le disque dur externe
+jailDir=""
+HDDDir=""
+
+if [ -d $jailDir ]; then
+
+	if [ -d $HDDDir ]; then
+
+		rsync -avz --progress $jailDir"/Rushes/*" $HDDDir"/Rushes/"
+
+		echo -e "Terminé"
+	else
+		echo "Le dossier "$HDDDir" n'existe pas."
+
+else
+	echo "Le dossier "$jailDir" n'existe pas."
+fi
